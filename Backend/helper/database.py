250b017aa1df5f1ce4994a6f40c1adfb1ebe2a07
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2025-11-18 14:36:39 +0530

    Update database.py

--- Backend/helper/database.py
+++ Backend/helper/database.py
@@ -26,7 +26,7 @@ def convert_objectid_to_str(document: Dict[str, Any]) -> Dict[str, Any]:
 
 
 class Database:
-    def __init__(self, db_name: str = "dbFyvio"):
+    def __init__(self, db_name: str = "dbFyvio_backup_data"):
         self.db_uris = Telegram.DATABASE
         self.db_name = db_name
 
